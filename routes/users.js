100ab744208ffeefd344cece68b7cf1f75e6b6d1
Saad Farooq <farooqsaad2810@gmail.com>
2023-01-20 21:44:59 +0500

    feat: added user validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @route     Get api/users
-// @desc      Test Route
+// @route     POST api/users
+// @desc      Register Route
 // @access    Public
-router.get('/', (req, res) => {
- res.send('user Routes');
-});
+
+router.post(
+ '/',
+ [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please include a valid email address').isEmail(),
+  check(
+   'password',
+   'Please enter a password in range of 6 to 100 characters'
+  ).isLength({ min: 6, max: 100 }),
+ ],
+
+ (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+   return res.status(400).json({ errors: errors.array() });
+  }
+  console.log(req.body);
+  res.send('user Routes');
+ }
+);
 
 module.exports = router;
