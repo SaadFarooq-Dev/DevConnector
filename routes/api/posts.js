86add00633720b801870d632a52022a71f394675
Saad Farooq <farooqsaad2810@gmail.com>
2023-01-30 19:58:47 +0500

    feat: Added post & delete comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -69,6 +69,37 @@ router.post(
  }
 );
 
+// @route     POST api/posts/comment/:id
+// @desc      Create a post comment
+// @access    Private
+
+router.post(
+ '/comment/:id',
+ [auth, [check('text', 'Text is required').not().isEmpty()]],
+ async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+   return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+   const user = await UserModel.findById(req.user.id).select('-password');
+   const post = await PostModel.findById(req.params.id);
+   const newComment = {
+    text: req.body.text,
+    name: user.name,
+    avatar: user.avatar,
+    user: req.user.id,
+   };
+   post.comments.unshift(newComment);
+   await post.save();
+   res.json(post.comments);
+  } catch (error) {
+   console.error(error.message);
+   res.status(500).json('Server Error: ' + error.message);
+  }
+ }
+);
+
 // @route     PUT api/posts/like/:id
 // @desc      Create a post Like relationship
 // @access    Private
@@ -110,7 +141,7 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 
   await PostModel.findOneAndUpdate(
-   { user: req.user.id, _id: req.params.id, 'likes.user': req.user.id },
+   { _id: req.params.id, 'likes.user': req.user.id },
    {
     $pull: {
      likes: { user: req.user.id },
@@ -149,6 +180,39 @@ router.delete('/:id', auth, async (req, res) => {
    return res.status(404).json({ msg: 'There is no such post' });
   }
   res.status(500).send('Server Error');
+ }
+});
+
+// @route     POST api/posts/:post_id/comment/:comment_id
+// @desc      Delete a comment
+// @access    Private
+
+router.delete('/:post_id/comment/:comment_id', auth, async (req, res) => {
+ try {
+  const post = await PostModel.findById(req.params.post_id);
+
+  const comment = post.comments.find(
+   (comment) => comment.id === req.params.comment_id
+  );
+  if (!comment) {
+   return res.status(404).json({ msg: 'Comment not found' });
+  }
+  if (comment.user.toString() !== req.user.id) {
+   return res.status(404).json({ msg: 'User not Authorized' });
+  }
+  await PostModel.updateOne(
+   { _id: req.params.post_id },
+   {
+    $pull: {
+     comments: { _id: req.params.comment_id },
+    },
+   }
+  );
+
+  res.json({ msg: 'Comment Removed', comment: comment });
+ } catch (error) {
+  console.error(error.message);
+  res.status(500).json('Server Error: ' + error.message);
  }
 });
 
